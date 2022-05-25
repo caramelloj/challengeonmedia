4b003c60956d90a3212da22a099bff9cc929e2f2
caramelloj <caramellojose54@gmail.com>
2022-05-25 00:51:50 -0300

    rm unusefulfunctions

--- app/Http/Controllers/DatatableController.php
+++ app/Http/Controllers/DatatableController.php
@@ -10,27 +10,6 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class DatatableController extends Controller
 {
-    public function onevsone(){
-        $response = Http::get('https://aoe2.net/api/leaderboard?game=aoe2de&leaderboard_id=3&start=1&count=100');
- 
-        $resp = $response->json();
-        
-        $players1v1 = $resp['leaderboard'];
-
-        return datatables()->of($players1v1)->toJson();
-    }
-
-    public function elotg(){
-        $response1 = Http::get('https://aoe2.net/api/leaderboard?game=aoe2de&leaderboard_id=2&start=1&count=100');
-
-        $resp1 = $response1->json();
-
-        $tg = $resp1['leaderboard'];
-
-        return datatables()->of($tg)->toJson();
-
-    }
-
     public function exportxls(){
         return Excel::download(new TablesExport, 'listadojugadores.xlsx');
     }
