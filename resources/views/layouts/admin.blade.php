d2d51e6fbd66578266c7c69833384d00e25cfbed
caramelloj <caramellojose54@gmail.com>
2022-05-26 11:59:34 -0300

    edit footer

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -147,7 +147,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
+                        <span>Copyright &copy; made by n1ck 2021</span>
                     </div>
                 </div>
             </footer>
